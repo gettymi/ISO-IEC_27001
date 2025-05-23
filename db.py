6c8d125ded1f84e6a6b9f0a74c0b4d5c084b8fdd
gettymi <bogdantereshchenko3@gmail.com>
2025-05-23 08:24:06 +0200

    Create db.py

---
+++ db.py
@@ -0,0 +1,15 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("PGHOST"),
+        database=os.getenv("PGDATABASE"),
+        user=os.getenv("PGUSER"),
+        password=os.getenv("PGPASSWORD"),
+        port=os.getenv("PGPORT", 5432),
+        sslmode="require"
+    )
